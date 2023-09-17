b54f8a9be26ce3f4e3175f99e8cd6ed4fa064657
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-17 20:21:51 +0300

    add class User

--- src/main/java/by/aleksandrphilimonov/jwtappdemo/model/Role.java
+++ src/main/java/by/aleksandrphilimonov/jwtappdemo/model/Role.java
@@ -2,14 +2,17 @@ package by.aleksandrphilimonov.jwtappdemo.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "roles")
 @Data
 public class Role extends BaseEntity {
+
     @Column(name = "name")
     private String name;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private List<User> users;
 }
