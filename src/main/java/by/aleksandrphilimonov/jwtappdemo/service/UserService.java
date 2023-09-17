63906c67e80b010f9e70c2826d9af8ed67ab5647
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-17 20:31:32 +0300

    add layer service with interface UserService

---
+++ src/main/java/by/aleksandrphilimonov/jwtappdemo/service/UserService.java
@@ -0,0 +1,18 @@
+package by.aleksandrphilimonov.jwtappdemo.service;
+
+import by.aleksandrphilimonov.jwtappdemo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User register(User user);
+
+    List<User> getAll();
+
+    User findByUserName(String username);
+
+    User findById(Long id);
+
+    void delete(Long id);
+}
