2ac230696e913bb5c2bf00a11899655c41ab131f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-17 23:36:33 +0300

    add layer security, jwt

--- src/main/java/by/aleksandrphilimonov/jwtappdemo/JwtAppDemoApplication.java
+++ src/main/java/by/aleksandrphilimonov/jwtappdemo/JwtAppDemoApplication.java
@@ -2,19 +2,12 @@ package by.aleksandrphilimonov.jwtappdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class JwtAppDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtAppDemoApplication.class, args);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
